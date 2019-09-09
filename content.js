f92be588ded688d6c833db7e5d85ff0caff81430
Sebastian Portuondo <slportuondo@gmail.com>
2019-09-09 15:29:59 -0500

    works with certain images
    https://www.google.com/search?q=image&rlz=1C5CHFA_enUS706US707&sxsrf=ACYBGNS13_fWhsDQ1E6QFi-sXFm1BHbOhw:1568037039945&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiisYqW8cPkAhUHC6wKHfpkAoUQ_AUIEigB&biw=1440&bih=788#imgrc=_

--- content.js
+++ content.js
@@ -1,10 +1,24 @@
-let imageTags = document.getElementsByTagName("img")
+let imageTags = document.getElementsByTagName('img');
+
 
 [...imageTags].forEach(function(tag){
-  tag.addEventListener('dragstart', () => {
-    console.log('%c ITS WORKING', 'color: blue;');
+  tag.addEventListener('dragstart', async (e) => {
+    const photoSrcToDrop = tag.getAttribute('src')
+    const foundOnURL = location.href;
+
+    (async () => {
+      console.log('about to make post request');
+      const res = await fetch('http://localhost:3000/drop/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({srcUrl: photoSrcToDrop, foundOnURL: foundOnURL})
+      });
+      console.log('after post request')
+      const content = await res.json()
+      console.log(content)
+    })()
   }, true)
 })
-
-
-// chrome.runtime.sendMessage(document.getElementsByTagName('img')[0].innerText)
