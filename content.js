481da40c6e5e26c9c790b8d057259edc5b1e0887
Sebastian Portuondo <slportuondo@gmail.com>
2019-09-15 21:07:27 -0500

    content script can send and receive data to the background script

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
 // add event listeners to each image on the page --> temporary fix to get around chrome security setbacks
 let imageTags = document.getElementsByTagName('img');
 
+
 [...imageTags].forEach(function(tag){
-  tag.addEventListener('dragstart', async (e) => {
+
+  tag.addEventListener('dragstart', async () => {
     const srcURL = tag.getAttribute('src');
     const foundOnURL = window.location.href;
+
+    chrome.runtime.sendMessage('kdbjhmeefcbnibecoeljilndpplpjpee',{ type: 'sending', message: 'sending'});
 
     // drop the image (in the form of it's src attribute and the location it was found) to the express server
     (async () => {
@@ -14,10 +18,11 @@ let imageTags = document.getElementsByTagName('img');
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({srcURL: srcURL, foundOnURL: foundOnURL})
+        body: JSON.stringify({srcURL: srcURL})
       });
-
-      console.log(successfulDrop);
     })()
+
+    // send the website the image was found on to the popup, indicating that it was sent
+    chrome.runtime.sendMessage('kdbjhmeefcbnibecoeljilndpplpjpee', {type: 'sent', message: foundOnURL});
   }, true)
 })
