57e6a3dc87ae0203fbabc638096c303fca231a6e
Sebastian Portuondo <slportuondo@gmail.com>
2019-09-19 17:44:01 -0500

    it takes a new session name in the popup, sends session name to background script

--- popup.js
+++ popup.js
@@ -1,18 +1,21 @@
-document.getElementById('currentKey').addEventListener('click', function(e) {
-  if (e == 1) {
+// function to send click data from popup to background.js --> fetch requests cannot be made from the popup script
+
+const sendMessage = (type, message = null) => {
+  chrome.runtime.sendMessage({type: type, message: message});
+}
+
+document.getElementById('dropdownButtons').addEventListener('click', async (e) => {
+  const clicked = e.target.id
+  if (clicked == 'newSessionButton') {
+    const userInput = document.getElementById('newSessionNameInput')
+    const newSessionName = userInput.value
+    userInput.value = ''
+
+    const returnedKey = await sendMessage('newSession', newSessionName);
+
+  } else if (clicked == 'currentKeyButton') {
     // Request current key
-    async () => {
-      const returnedKey = await chrome.runtime.sendMessage({type: 'getCurrentKey'});
-    }
+    const returnedKey = await sendMessage('getCurrentKey')
+
   }
 });
-
-
-
-
-
-
-
-// const newSession = (id) => {
-//
-// }
